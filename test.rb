82957dde310dc161c2a8ebf25208624ae949dc65
Brandon <bmoore320@yahoo.com>
2019-02-21 10:43:11 -0800

    adds array pre-work code

--- test.rb
+++ test.rb
@@ -1,1 +1,39 @@
-change 4
+# fruits = ["apple", "banana", "kiwi", "mango", ]
+
+# fruits.each do |fruit|
+#   p fruit
+# end
+
+
+
+
+array = [5,3,1,7,6,4,2,8]   
+
+sort0 = 0
+sort1 = 1
+
+7.times do
+  7.times do                              
+    if array[sort0
+  ] > array[sort1]        
+      temp = array[sort0
+    ]                  
+      array[sort0
+    ] = array[sort1]          
+      array[sort1] = temp                  
+      sort0
+     = sort0
+     + 1                    
+      sort1 = sort1 + 1                    
+    else
+    sort0
+   = sort0
+   + 1
+    sort1 = sort1 + 1
+    end
+  end
+sort0 = 0
+sort1 = 1
+end
+
+p array
